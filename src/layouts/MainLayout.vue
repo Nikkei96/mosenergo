498c52a9821b7b12b0559297f9f98329cf3c8e10
Nikkei96 <nikitarudentsov@yandex.ru>
2021-12-05 00:09:24 +0300

    add base structure

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+  data: () => ({
+    cards: ["Today", "Yesterday"],
+    drawer: null,
+    links: [
+      ["mdi-home", "Главная", "/"],
+      ["mdi-chart-areaspline", "Графики", "/charts"],
+    ],
+  }),
+};
+</script>
+
+<template>
+  <v-app id="inspire">
+    <v-system-bar app>
+      <v-spacer></v-spacer>
+
+      <v-icon>mdi-square</v-icon>
+
+      <v-icon>mdi-circle</v-icon>
+
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar>
+
+    <v-navigation-drawer v-model="drawer" app>
+      <v-sheet color="grey lighten-4" class="pa-4">
+        <v-avatar class="mb-4" color="grey darken-1" size="64">
+          <img
+            src="https://w7.pngwing.com/pngs/980/886/png-transparent-male-portrait-avatar-computer-icons-icon-design-avatar-flat-face-icon-people-head-cartoon.png"
+            alt="John"
+          />
+        </v-avatar>
+
+        <div>admin@mosenergo.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item v-for="[icon, text, path] in links" :key="icon" link :to="path">
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container class="py-8 px-6" fluid>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
